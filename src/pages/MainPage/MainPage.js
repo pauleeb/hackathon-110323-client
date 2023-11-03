1df27f8d1bb6316a2b06a67e62870ed447b82238
Emily Kusich <emilykusich@Emilys-Air.localdomain>
2023-11-03 11:43:41 -0400

    GIT PROS

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,77 @@
+import './MainPage.scss';
+
+function MainPage() {
+    return (
+        <>
+            <form>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+                <div className='question-container'>
+                    <h2 className='question'>Question
+                    </h2>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                    <button className='button'>Choice</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+
+export default MainPage;
